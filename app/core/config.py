0a15722f3b02c88b107c20349bb705ea43ec9f86
Станислав <morevstas02@mail.ru>
2025-08-01 12:19:03 +0300

    Создание бд + подключение

---
+++ app/core/config.py
@@ -0,0 +1,33 @@
+import os
+
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+
+load_dotenv()
+
+
+class Settings(BaseSettings):
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+    # SECRETS: str
+    model_config = SettingsConfigDict(
+        env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), ".env")
+    )
+
+    def get_db_url(self):
+        return (f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@"
+                f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}")
+
+settings = Settings()
+
+
+database_url = settings.get_db_url()
+print(database_url)
+
+
+engine = create_async_engine(url=database_url)
+async_session_maker = async_sessionmaker(engine, class_=AsyncSession,expire_on_commit=False)
