a4892f32153256c05078e62bf2e9bf90dd0d6c6b
Станислав <morevstas02@mail.ru>
2025-08-02 18:57:45 +0300

    главный файл

---
+++ app/main.py
@@ -0,0 +1,21 @@
+import uvicorn
+from fastapi import FastAPI
+
+from app.api.admin import admin_router
+
+app = FastAPI()
+
+
+
+# app.include_router(auth.router, prefix="/auth", tags=["auth"])
+# app.include_router(products.router, prefix="/products", tags=["products"])
+# app.include_router(cart.router, prefix="/cart", tags=["cart"])
+# app.include_router(orders.router, prefix="/orders", tags=["orders"])
+app.include_router(admin_router)
+
+
+
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app',reload=True)
