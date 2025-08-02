688628d21acf5d65d9d610886c4f54c73c271959
Станислав <morevstas02@mail.ru>
2025-08-02 18:57:10 +0300

    соединение с базой

---
+++ app/deps/dependes.py
@@ -0,0 +1,8 @@
+from typing import AsyncGenerator
+
+from app.core.config import async_session_maker
+
+
+async def get_db() -> AsyncGenerator:
+    async with async_session_maker() as session:
+        yield session
