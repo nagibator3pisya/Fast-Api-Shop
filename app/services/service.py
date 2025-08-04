4f7042849c33deb48e268e1f4eb27d14f6f0e6f4
Станислав <morevstas02@mail.ru>
2025-08-04 12:21:20 +0300

    вывод категорий, продуктов

--- app/services/service.py
+++ app/services/service.py
@@ -1,3 +1,14 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.model.models import Product, Category
 
 
+async def all_product(session: AsyncSession):
+    result = await session.execute(select(Product))
+    return result.scalars().all()
 
+
+async def all_category(session: AsyncSession):
+    result = await session.execute(select(Category))
+    return result.scalars().all()
