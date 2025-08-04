0811a7964c2a640d731debd46274c6fb29d427e3
Станислав <morevstas02@mail.ru>
2025-08-05 00:42:56 +0300

    настройки JWT

---
+++ app/api/auth.py
@@ -0,0 +1,86 @@
+from datetime import timedelta, datetime
+from typing import Optional
+
+from fastapi import Depends, HTTPException,status
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
+from passlib.context import CryptContext
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Session
+
+from app.core.config import settings
+from app.deps.dependes import get_db
+from app.model.models import User
+from app.schemas.Token import TokenData
+
+from app.services.service_auth import test, get_user_by_email
+
+SECRET_KEY = settings.SECRET_KEY
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/auth/login/')
+
+
+def verify_password(plain_password,hashed_password):
+    return pwd_context.verify(plain_password,hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now() + expires_delta
+    else:
+        expire = datetime.now() + timedelta(minutes=15)
+
+    to_encode.update({'exp': expire})
+    encoded_jwt = jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def verify_token(token:str, credentials_exception):
+    try:
+        payload = jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    return token_data
+
+
+async def authenticate_user(session: AsyncSession, email: str, password: str):
+    user = await get_user_by_email(session=session, email=email)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme), session: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Не удалось проверить учетные данные",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    token_data = verify_token(token, credentials_exception)
+
+    tests = await test(token_data=token_data, session=session)
+
+    if tests is None:
+        raise credentials_exception
+    return tests
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if not current_user.is_active:
+        raise HTTPException(status_code=400, detail="Неактивный пользователь")
+    return current_user
