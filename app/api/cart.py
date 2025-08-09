eb3f716fa14521b937bfdf9488b2412538526e70
Станислав <morevstas02@mail.ru>
2025-08-09 14:46:39 +0300

    удаление тавара + обновление количества

--- app/api/cart.py
+++ app/api/cart.py
@@ -1,12 +1,14 @@
 from fastapi import APIRouter
 from fastapi.params import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.util import await_only
 
 from app.api.auth import get_current_user
 from app.deps.dependes import get_db
 from app.model.models import User
 from app.schemas.CartItem import CartItemOut, CartItemCreate
 from app.services import service
+from app.services.service import remove_from_cart, changes_quantity_cart
 
 cart_router = APIRouter(prefix="/cart", tags=["cart"])
 
@@ -25,14 +27,23 @@ async def add_cart(schemas_app: CartItemCreate, session: AsyncSession = Depends(
     return result
 
 
-# @cart_router.put('/change_quantity/')
-# async def change_quantity_cart(quantity_id: int):
-#     ...
-#
-#
-# @cart_router.delete('/delete_product/')
-# async def delete_to_product_cart(product_id: int):
-#     ...
+
+@cart_router.delete("/remove_from_cart/{cart_item_id}")
+async def delete_cart(cart_item_id: int, session: AsyncSession = Depends(get_db), current_user: User = Depends(get_current_user)):
+    return await remove_from_cart(session=session, cart_item_id=cart_item_id, current_user=current_user)
+
+
+
+@cart_router.put('/change_quantity/{cart_item_id}/')
+async def change_quantity_cart(quantity_id: int,
+                               new_quantity: int,
+                               current_user: User = Depends(get_current_user),
+                               session: AsyncSession = Depends(get_db)):
+    return await changes_quantity_cart(session=session, quantity_id=quantity_id,
+                                       new_quantity=new_quantity, current_user=current_user)
+
+
+
 #
 #
 # @cart_router.post('/checkout/')
