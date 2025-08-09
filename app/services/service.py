e79793f50b4857a35111a43d6d168a71117a417b
Станислав <morevstas02@mail.ru>
2025-08-09 13:23:31 +0300

    Уменьшение товара на складе + запрос на удаление из карзины и доабвление на склад

--- app/services/service.py
+++ app/services/service.py
@@ -1,6 +1,8 @@
+from fastapi import HTTPException
 from fastapi.params import Depends
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
 
 from app.api.auth import get_current_user
 from app.model.models import Product, Category, CartItem, User
@@ -31,10 +33,69 @@ async def my_cart_all(session: AsyncSession,  current_user: User = Depends(get_c
 '''
 
 
-
 async def add_to_cart(session: AsyncSession, schemas: CartItemCreate, user: User):
-    cart_items = CartItem(**schemas.model_dump(), user_id=user.id)
-    session.add(cart_items)
+    # 1. Проверка остатка товара
+    query = select(Product).where(Product.id == schemas.product_id)
+    result = await session.execute(query)
+    product = result.scalars().first()
+
+    if not product:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Товар не найден"
+        )
+
+    if product.quantity < schemas.quantity:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"На складе осталось только {product.quantity} шт."
+        )
+
+
+    # уменьшение остатка на складе
+    product.quantity -= schemas.quantity
+    session.add(product)
+
+
+    # 2. Добавление в корзину
+    cart_item = CartItem(
+        **schemas.model_dump(),
+        user_id=user.id
+    )
+    session.add(cart_item)
     await session.commit()
-    await session.refresh(cart_items)
-    return cart_items
+    await session.refresh(cart_item)
+
+    return cart_item
+
+
+
+async def remove_from_cart(session: AsyncSession, cart_item_id: int, user: User):
+    # 1. Поиск товара в карзине
+    query = select(CartItem).where(
+        CartItem.id == cart_item_id,
+        CartItem.user_id == user.id
+    )
+    result = await session.execute(query)
+    cart_item = result.scalars().first()
+
+    if not cart_item:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Товар в корзине не найден"
+        )
+
+    # 2. Возвращаем товар на склад
+    query = select(Product).where(Product.id == cart_item.product_id)
+    result = await session.execute(query)
+    product = result.scalars().first()
+
+    if product:
+        product.quantity += cart_item.quantity
+        session.add(product)
+
+    # 3. Удаляем из корзины
+    await session.delete(cart_item)
+    await session.commit()
+
+    return {"detail": "Товар удалён из корзины и резерв снят"}
