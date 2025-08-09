a5864a6cbfeb9f19b5a4ed895153bc22e79502ff
Станислав <morevstas02@mail.ru>
2025-08-09 14:47:10 +0300

    запрос удаление тавара + обновление количества

--- app/services/service.py
+++ app/services/service.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from fastapi import HTTPException
 from fastapi.params import Depends
 from sqlalchemy import select
@@ -34,47 +36,37 @@ async def my_cart_all(session: AsyncSession,  current_user: User = Depends(get_c
 
 
 async def add_to_cart(session: AsyncSession, schemas: CartItemCreate, user: User):
-    # 1. Проверка остатка товара
     query = select(Product).where(Product.id == schemas.product_id)
     result = await session.execute(query)
     product = result.scalars().first()
 
     if not product:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Товар не найден"
-        )
+        raise HTTPException(status_code=404, detail="Товар не найден")
 
     if product.quantity < schemas.quantity:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=f"На складе осталось только {product.quantity} шт."
-        )
+        raise HTTPException(status_code=400, detail=f"На складе осталось {product.quantity} шт.")
 
-
-    # уменьшение остатка на складе
+    # резервируем
     product.quantity -= schemas.quantity
-    session.add(product)
-
-
-    # 2. Добавление в корзину
     cart_item = CartItem(
-        **schemas.model_dump(),
-        user_id=user.id
+        product_id=schemas.product_id,
+        quantity=schemas.quantity,
+        user_id=user.id,
+        reserved_until=datetime.utcnow() + timedelta(minutes=15)
     )
+    session.add(product)
     session.add(cart_item)
     await session.commit()
     await session.refresh(cart_item)
-
     return cart_item
 
 
 
-async def remove_from_cart(session: AsyncSession, cart_item_id: int, user: User):
+async def remove_from_cart(session: AsyncSession, cart_item_id: int, current_user: User = Depends(get_current_user)):
     # 1. Поиск товара в карзине
     query = select(CartItem).where(
         CartItem.id == cart_item_id,
-        CartItem.user_id == user.id
+        CartItem.user_id == current_user.id
     )
     result = await session.execute(query)
     cart_item = result.scalars().first()
@@ -99,3 +91,79 @@ async def remove_from_cart(session: AsyncSession, cart_item_id: int, user: User)
     await session.commit()
 
     return {"detail": "Товар удалён из корзины и резерв снят"}
+
+
+async def changes_quantity_cart(session: AsyncSession,
+                                quantity_id: int,
+                                new_quantity: int,
+                                current_user: User = Depends(get_current_user)):
+    # 1. Поиск товара в карзине
+    query = select(CartItem).where(
+        CartItem.id == quantity_id,
+        CartItem.user_id == current_user.id
+    )
+    result = await session.execute(query)
+    cart_item = result.scalars().first()
+
+    if not cart_item:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Товар в корзине не найден"
+        )
+
+    product = await session.get(Product, cart_item.product_id)
+    if not product:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Товар не найден или недоступен"
+        )
+
+
+    #  Если новое количество больше, чем в корзине — зарезервировать доп. товары
+    if new_quantity > cart_item.quantity:
+        diff = new_quantity - cart_item.quantity
+        if product.quantity < diff:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f"На складе осталось только {product.quantity} шт."
+            )
+        product.quantity -= diff # резервируется
+
+    # Если количество уменьшилось - вернуть на склад
+
+    elif new_quantity < cart_item.quantity:
+        diff = cart_item.quantity - new_quantity
+        product.quantity += diff
+
+
+    cart_item.quantity = new_quantity
+    session.add_all([cart_item,product])
+    await session.commit()
+    await session.refresh(cart_item)
+
+    return {"detail": "Количество товара обновлено", "cart_item": cart_item}
+
+
+
+
+
+
+
+async def clear_expired_reservations(session: AsyncSession):
+    '''
+    Очистка просроченных резервов
+    :param session:
+    :return:
+    '''
+    query = select(CartItem).where(CartItem.reserved_until < datetime.utcnow())
+    result = await session.execute(query)
+    expired_items = result.scalars().all()
+
+    for item in expired_items:
+        product = await session.get(Product, item.product_id)
+        if product:
+            product.quantity += item.quantity
+            session.add(product)
+        await session.delete(item)
+
+    await session.commit()
